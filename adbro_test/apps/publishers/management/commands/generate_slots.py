b63590e32d5c1084208bcc8ab029c74872ee0387
Denanda Hendra <denandahp@gmail.com>
2023-01-21 19:15:44 +0700

    Comment generate slots

--- adbro_test/apps/publishers/management/commands/generate_slots.py
+++ adbro_test/apps/publishers/management/commands/generate_slots.py
@@ -6,30 +6,36 @@ from adbro_test.apps.publishers.model import Site, Slot, Publisher
 from faker import Faker
 
 class Command(BaseCommand):
+    '''
+    This command is to creates 10 publisher with 1-3 site for each,
+    and every site create 1-4 slot.
+    '''
     def handle(self, *args, **options):
         faker = Faker()
 
         with transaction.atomic():
-            # Create publishers
+            # Create 10 publishers
             publishers_list = []
             for i in range(10):
                 publisher = Publisher(name=faker.name())
                 publishers_list.append(publisher)
-            
+
+            # Validate if publishers_list is exist
             if publishers_list:
                 publishers = Publisher.objects.bulk_create(publishers_list)
 
-                # Create sites for each publisher
+                # Create 1-3 sites for each publisher
                 sites_list = []
                 for publisher in publishers:
                     for i in range(faker.random_int(1, 3)):
                         site = Site(publisher=publisher, name=faker.company())
                         sites_list.append(site)
             
+            # Validate if sites_list is exist
             if sites_list:
                 sites = Site.objects.bulk_create(sites_list)
 
-                # Create slots for each site
+                # Create 1-10 slots for each site
                 slots_list = []
                 for site in sites:
                     for i in range(faker.random_int(1, 10)):
